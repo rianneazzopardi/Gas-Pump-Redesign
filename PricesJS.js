6ba386f8270c9934f0a8a307fc7c4beec23260d8
Stephc02 <103242667+Stephc02@users.noreply.github.com>
2023-01-04 10:08:01 +0100

    Update PricesJS.js
    removed extra comments

--- PricesJS.js
+++ PricesJS.js
@@ -1,22 +1,26 @@
-//Get All Value
+// Stephanie
+
+//Get all values
+
 let sendBtn = document.getElementById('send');
 let form = document.getElementById('form');
 
-//Form Refresh State
+//Form refresh state
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
 
 //Now Start SendBtn
-sendBtn.addEventListener('click', (e) => {
-    let regularpetrol = document.getElementById('regularpetrol'); // was name 
-    let diesel = document.getElementById('diesel');
-    // was email 
-    let higradepetrol = document.getElementById('higradepetrol');
-    // was message
 
-    //Set Value And LocalStorage
+sendBtn.addEventListener('click', (e) => {
+    let regularpetrol = document.getElementById('regularpetrol');  
+    let diesel = document.getElementById('diesel');
+    let higradepetrol = document.getElementById('higradepetrol');
+   
+
+    //Set values and LocalStorage
     
     regularpetrol = regularpetrol.value;
     localStorage.setItem('regularpetrol', regularpetrol);
