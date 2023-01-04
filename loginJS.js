b0114befdf691c58985575f8ffb632c964789c1e
Stephc02 <103242667+Stephc02@users.noreply.github.com>
2023-01-04 10:01:52 +0100

    Update loginJS.js
    fixed indentation

--- loginJS.js
+++ loginJS.js
@@ -1,57 +1,60 @@
 // Stephanie 
 
-function hide(){
-        var x = document.getElementById('hide1'); 
-        var y = document.getElementById('hide2');
-        var w = document.getElementById('pass');
+function hide()
+{
+    var x = document.getElementById('hide1'); 
+    var y = document.getElementById('hide2');
+    var w = document.getElementById('pass');
 
-        if (w.type === 'password'){
-            w.type = "text";
-            x.style.display='block';
-            y.style.display='none';
-        }else{
-            w.type = "password";
-            x.style.display='none';
-            y.style.display='block';
-        }
+    if (w.type === 'password')
+    {
+        w.type = "text";
+        x.style.display='block';
+        y.style.display='none';
+    }
+    else
+    {
+        w.type = "password";
+        x.style.display='none';
+        y.style.display='block';
+    }
 }
 
-function log(){
+function log()
+{
    
     let userP = localStorage.getItem("password");
     let inpP = document.getElementById('pass').value;
 
-    if(inpP == userP){
+    if(inpP == userP)
+    {
         window.location.assign("manager.html") 
         
-
     }
-    else{
-    document.getElementById('error').style.display='block';
-    document.getElementById('error').innerHTML = "Incorrect Information";
+    else
+    {
+        document.getElementById('error').style.display='block';
+        document.getElementById('error').innerHTML = "Incorrect Information";
+    }
+}
 
 
-}}
-
-
-function log2(){
+function log2()
+{
    
     let userP = localStorage.getItem("password");
     let inpP = document.getElementById('pass').value;
 
-    if(inpP == userP){
+    if(inpP == userP)
+    {
         window.location.assign("manager.html") 
         
 
     }
-    else{
-    document.getElementById('error').style.display='block';
-    document.getElementById('error').innerHTML = "Incorrect Information";
-
-
-}}
-
-
-
-
+    else
+    {
+        document.getElementById('error').style.display='block';
+        document.getElementById('error').innerHTML = "Incorrect Information";
+    }
+}
 
